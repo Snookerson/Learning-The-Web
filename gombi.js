b5b4e0779ced8101839b286619e9a46c1e602967
Snookerson <89213326+Snookerson@users.noreply.github.com>
2021-09-13 09:59:04 +0200

    Patch 7.1 - 2021.09.13.
    - Fixed a misspell
    - Added instagram logo + link in to the footer
    - Fully working dark and light theme with button

--- gombi.js
+++ gombi.js
@@ -1,60 +1,84 @@
+/* This section is about setting up the dark or light theme by a button press */
 
-
-
-/* Accessing the :root from CCS, so we can give it another style */
+/* We get the :root from the CSS */
 const root = document.documentElement;
-const darkThemeBtn = document.getElementById("darkTheme");
-const lightThemeBtn = document.getElementById("lightTheme");
 
+/* We find the button in the HTML by classname so later we can check if the id is "dark" or "light".
+This is the way we are gonna know which theme to applie
+If id == light  --> apply dark.
+If id == dark  --> apply light. */
+const themeBtn = document.getElementsByClassName("theme-selector-btn")[0];
 
-/* Set up dark theme */
-if (darkThemeBtn != null) {
-    darkThemeBtn.addEventListener("click", () => {
+/* Creating a clickevent with a pre defind function - which contains the stlyle and ID changes */
+themeBtn.addEventListener("click", themeChanger);
+
+function themeChanger() {
+    if (themeBtn.id == "darkTheme") {
+            root.style.setProperty
+            ("--background-color", "hsl(0, 0%, 16%)");
+            
+            root.style.setProperty
+            ("--text-color", "hsla(0, 0%, 100%, 0.9)");
+    
+            root.style.setProperty
+            ("--header-color", "hsl(0, 0%, 9%)");
+    
+            root.style.setProperty
+            ("--btn-color", "hsl(0, 0%, 12%)");
+    
+            root.style.setProperty
+            ("--mobile-btn-color", "hsl(0, 0%, 12%)");
+    
+            root.style.setProperty
+            ("--article-background-color", "hsl(0, 0%, 19%)");
+
+            root.style.setProperty
+            ("--btn-theme-background", "hsla(0, 0%, 5%, 0.9)")
+
+            root.style.setProperty
+            ("--footer-color", "hsl(0, 0%, 9%)")
+
+            /* Here we find the theme img to change the icon */
+            document.getElementById("themeIcon").src = "Images/light theme logo.svg";
+
+            /* Here we find the insta img to change the icon */
+            document.getElementById("insta").src = "Images/dark-insta-logo.png";
+            
+            /* Setting (changing the html) a new id so the button could work multiple times*/
+            themeBtn.id = "lightTheme";
+
+        
+
+    } else if (themeBtn.id == "lightTheme") {
         root.style.setProperty
-        ("--background-color", "hsl(0, 0%, 16%)");
+        ("--background-color", "linear-gradient(180deg, rgba(104,141,135,1) 0%, rgba(68,90,112,1) 100%)");
         
         root.style.setProperty
-        ("--text-color", "hsla(0, 0%, 100%, 0.9)");
+        ("--text-color", "hsla(0, 0%, 5%, 0.9)");
 
         root.style.setProperty
-        ("--header-color", "hsl(0, 0%, 9%)");
+        ("--header-color", "hsl(171, 15%, 46%)");
 
         root.style.setProperty
-        ("--btn-color", "hsl(0, 0%, 12%)");
+        ("--btn-color", "linear-gradient(180deg, rgba(100,135,130,1) 0%, rgba(126,170,163,1) 100%)");
 
         root.style.setProperty
-        ("--mobile-btn-color", "hsl(0, 0%, 12%)");
+        ("--mobile-btn-color", "hsl(171, 15%, 56%)");
 
         root.style.setProperty
-        ("--article-background-color", "hsl(0, 0%, 19%)");
+        ("--article-background-color", "hsl(0, 0%, 93%)");
+
+        root.style.setProperty
+        ("--btn-theme-background", "hsl(0, 0%, 93%)")
+
+        root.style.setProperty
+        ("--footer-color", "hsla(240, 100%, 99%, 0.2)")
         
-        /* Next steps
-            - change button elements ID to lightTheme
-            - change img element src to the light icon
-                */
+        document.getElementById("themeIcon").src = "Images/dark theme logo.svg";
 
-    })
-    } else  {
+        document.getElementById("insta").src = "Images/light-insta-logo.png";
 
-    /* Set up light theme */
-            lightThemeBtn.addEventListener("click", () => {
-                root.style.setProperty
-                ("--background-color", "linear-gradient(180deg, rgba(104,141,135,1) 0%, rgba(68,90,112,1) 100%);");
-                
-                root.style.setProperty
-                ("--text-color", "hsla(0, 0%, 5%, 0.9)");
+        themeBtn.id = "darkTheme";
 
-                root.style.setProperty
-                ("--header-color", "hsl(171, 15%, 46%)");
-
-                root.style.setProperty
-                ("--btn-color", "linear-gradient(180deg, rgba(100,135,130,1) 0%, rgba(126,170,163,1) 100%);");
-
-                root.style.setProperty
-                ("--mobile-btn-color", "hsl(171, 15%, 56%)");
-
-                root.style.setProperty
-                ("--article-background-color", "hsl(0, 0%, 93%)");
-                
-            })
     }
+}
